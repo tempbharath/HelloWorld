73d180aff5cdced8e88b251fcc903a77540fae8c
tempbharath <98141799+tempbharath@users.noreply.github.com>
2022-03-07 11:34:28 +0530

    Add files via upload

---
+++ src/main/java/com/example/jenkinsintegration/JenkinsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsIntegrationApplication.class, args);
+	}
+
+}
